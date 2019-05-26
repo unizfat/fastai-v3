2d72e2eb308d508475f0a2b6d1be9f08f465920a
davidfunberstein <33499808+davidfunberstein@users.noreply.github.com>
2019-05-26 23:02:01 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1DDrq_8ADUFJztIJoevAROTNTUvNF3SRb'
+export_file_url = 'https://drive.google.com/open?id=1DDrq_8ADUFJztIJoevAROTNTUvNF3SRb'
 export_file_name = 'stage-2.pth'
 
 classes = ['palm','l','fist','fist_moved','thumb','index','ok','palm_moved','c','down']
