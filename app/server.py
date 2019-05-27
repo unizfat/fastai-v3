9e6e27a4258f4aacbe01cbb4aa0048cefbb4a01e
davidfunberstein <33499808+davidfunberstein@users.noreply.github.com>
2019-05-27 23:22:45 +0300

    add cv2

--- app/server.py
+++ app/server.py
@@ -72,12 +72,9 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
-
+    data = await request.form()
+    img_bytes = await (data['file'].read())
+    image = cv2.imdecode(np.fromstring(img_bytes, np.uint8), 1)
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
