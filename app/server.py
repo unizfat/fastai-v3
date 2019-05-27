00d4d4a2fec9a1c93c0d6c5b637153315695ac6e
davidfunberstein <33499808+davidfunberstein@users.noreply.github.com>
2019-05-27 22:33:37 +0300

    change url

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/gjver8e2l4ee2z8/export.pkl?dl=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/upke6l8e6w05m61/deaf-language.pkl?dl=1'
+export_file_name = 'deaf-language.pkl'
 
 classes = ['01_palm',
  '02_l',
